0b28ab3d0e3ccc9b5707f67451eb4c38d3b671a9
Vaibhav Narayan Gole <vaibhavgole88@gmail.com>
2023-10-17 23:03:27 +0530

    javascript hoisting

---
+++ hoisting.js
@@ -0,0 +1,8 @@
+
+// javascript moves declaration of variables and functions on top
+
+hoistedVariable = 2;    // variable initialized first
+
+console.log(hoistedVariable);   // var used
+
+let hoistedVariable;    // var declared
